5c4104721b771048a891a475ca8d7634fbafc336
Ffiirrsstt <first.engineer15@gmail.com>
2023-08-13 22:53:04 +0700

    Update the page for "Not Found 404" error.

--- src/component/Notfound.tsx
+++ src/component/Notfound.tsx
@@ -1,29 +1,34 @@
 import { useData } from "../useContext/useContext";
 import Nav from "./Nav";
-
-import { MdSearchOff } from "react-icons/md";
+import Notresult from "./Notresult";
 
 export default function Notfound() {
   const { tab } = useData();
   return (
-    <div className="m-h100 w two">
+    <div className="w flex flex-col">
       <Nav />
-      <div className="flex w">
-        <div className="w80 flex bg-w h90">
-          <div className="flex flex-col box-contactmain bg-two bor-bottom bor-top">
-            <div className="flex bg txt-color h w flex1 p20px bor-top">
-              <h1 className="font-xl mb10px">Welcome to Floral Haven.</h1>
-              {tab()}
-              <MdSearchOff className="icon-nav txt-colorred" />
-            </div>
-            <div className="flex flex3 w h p20px txt-color">
-              <h2 className="font-m font-wl">
-                This page cannot be found. Error 404.
-              </h2>
-            </div>
-          </div>
-        </div>
+      <div className="w80">
+        <Notresult data={3} eye={false} />
       </div>
     </div>
+    // <div className="m-h100 w two">
+    //   <Nav />
+    //   <div className="flex w">
+    //     <div className="w80 flex bg-w h90">
+    //       <div className="flex flex-col box-contactmain bg-two bor-bottom bor-top">
+    //         <div className="flex bg txt-color h w flex1 p20px bor-top">
+    //           <h1 className="font-xl mb10px">Welcome to QuantumUnity.</h1>
+    //           {tab()}
+    //           <MdSearchOff className="icon-nav txt-colorred" />
+    //         </div>
+    //         <div className="flex flex3 w h txt-color">
+    //           <h2 className="font-m font-wl">
+    //             This page cannot be found. Error 404.
+    //           </h2>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
--- src/component/Notresult.tsx
+++ src/component/Notresult.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 
 import { FiSearch } from "react-icons/fi";
 
+import { MdSearchOff } from "react-icons/md";
+
 import { FiShoppingBag } from "react-icons/fi";
 import { TfiAnnouncement } from "react-icons/tfi";
 import { useData } from "../useContext/useContext";
@@ -9,7 +11,7 @@ import { useState } from "react";
 
 interface typeforprops {
   data: number;
-  //1 = cart
+  //1 = cart //2 = search
   eye: boolean;
 }
 
@@ -20,15 +22,24 @@ export default function Notresult(props: typeforprops) {
   const [detail, setDetail] = useState<boolean>(false);
   const [instructions, setInstructions] = useState<boolean>(false);
 
-  const txthead =
-    data === 1
-      ? "No items found in the shopping cart."
-      : "I apologize, but the system did not locate the item you are searching for.";
+  let txthead, txtdetail;
+  if (data === 1) {
+    txthead = "No items found in the shopping cart.";
+    txtdetail = `To add items to your shopping cart :`;
+  } else if (data === 2) {
+    txthead =
+      "I apologize, but the system did not locate the item you are searching for.";
+    txtdetail =
+      "Please modify the text or keywords for searching the item of your interest.";
+  } else {
+    txthead = "Welcome to QuantumUnity.";
+    txtdetail = `This page cannot be found. Error 404.`;
+  }
 
   return (
     <div className={`w flex box-noneitem ${!eye && "m-h100"}`}>
       <div className="flex-col flex box-none box-shadow">
-        <h1 className="font-xl w h flex1 head-none bg txt-color flex bor-top">
+        <h1 className="plr20px font-xl w h flex1 head-none bg txt-color flex bor-top">
           {txthead}
           {tab()}
           <TfiAnnouncement
@@ -36,29 +47,34 @@ export default function Notresult(props: typeforprops) {
             title={txthead}
           />
         </h1>
-        <h2 className="flex3 font-xl font-wl h w txt-bg flex flex-col bor-bottom ">
+        <h2 className="plr20px flex3 font-xl font-wl h w txt-bg flex flex-col bor-bottom ">
           <span className="flex w">
-            {data === 1
-              ? `To add items to your shopping cart :`
-              : "Please modify the text or keywords for searching the item of your interest."}
+            {txtdetail}
             <span>{tab()}</span>
             <Link to="/">
               {data === 1 ? (
                 <FiShoppingBag className="icon-nav icon-hideeye " />
-              ) : (
+              ) : data === 2 ? (
                 <FiSearch
                   onClick={() => setInstructions(!instructions)}
                   className="icon-nav icon-hideeye "
                   title="View search instructions."
                 />
+              ) : (
+                <MdSearchOff
+                  className="icon-nav icon-hideeye "
+                  title={txtdetail}
+                />
               )}
             </Link>
           </span>
           {instructions && (
-            <div className="w flex flex-col pl20px mt15px">
-              <div>
-                <ul className="flex j-between w">
-                  <h4 className={`font-wb ${!detail ? "font-m" : "font-xl"}`}>
+            <div className="w flex flex-col plr20px mt15px">
+              <div className="">
+                <div className="flex w j-between">
+                  <h4
+                    className={`font-wb ${!detail ? "font-m" : "font-ex-xl"}`}
+                  >
                     Specifying Search:
                   </h4>
                   <button
@@ -69,16 +85,22 @@ export default function Notresult(props: typeforprops) {
                     {/* btn-fontIncrease อยู่ Allproduct.css*/}
                     Increase font size.
                   </button>
+                </div>
+                <ul className="flex flex-col j-between w">
+                  <li
+                    className={`mt15px pl20px w ${
+                      !detail ? "font-m" : "font-ex-xl"
+                    }`}
+                  >
+                    Specify by the product name or model of the iPad you are
+                    interested in.
+                  </li>
+                  <li
+                    className={`pl20px w ${!detail ? "font-m" : "font-ex-xl"}`}
+                  >
+                    Search using the numerical model for easy retrieval.
+                  </li>
                 </ul>
-                <li
-                  className={`mt15px pl20px ${!detail ? "font-m" : "font-xl"}`}
-                >
-                  Specify by the product name or model of the iPad you are
-                  interested in.
-                </li>
-                <li className={`pl20px ${!detail ? "font-m" : "font-xl"}`}>
-                  Search using the numerical model for easy retrieval.
-                </li>
               </div>
             </div>
           )}
